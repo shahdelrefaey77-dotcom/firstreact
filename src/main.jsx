d27796be1ffa12a26b4aeadeb968d40213731b14
shahdelrefaey77-dotcom <shahdelrefaey77@gmail.com>
2025-12-18 11:12:50 -0700

    use HashRouter to fix white page on vercel

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 );
