51459550ed1bec58d6947745cd449c8aa768d50c
shahdelrefaey77-dotcom <shahdelrefaey77@gmail.com>
2025-12-18 10:00:39 -0700

    add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
